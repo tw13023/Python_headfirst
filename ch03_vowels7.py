a53ee2b8013ee3cff334a59e2ed5ec3d59295bea
tw13023 <tw13023@gmail.com>
2023-09-02 06:17:17 +0800

    ch3 codes

---
+++ ch03_vowels7.py
@@ -0,0 +1,12 @@
+vowels = {'a','e','i','o','u'}
+word = input('Provide a word to search for vowels: ')
+#found = []
+#for letter in word :
+#    if letter in vowels:
+#        if letter not in found:
+#            found.append(letter)
+#for letter in found:
+#    print(letter)
+found = vowels.intersection(set(word))
+for letter in found:
+    print(letter)
